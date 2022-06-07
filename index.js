653cdc06e01234410cbf5063b45cbd5388f7fc01
Marc Velez <marcvelez13@gmail.com>
2022-06-07 07:11:50 -0400

    commit tree 73ec0dbf897aa59a59ef8d94851bc08c9c101f81

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+        return 'This one is on me!'
+      }
+       if ( ride > 400  ){
+        return 'That will be twenty bucks.'
+      }
+       if ( ride > 2001  ){
+        return 'I will gladly take your thirty bucks.'
+      }
+       if ( ride > 2500 ){
+        return 'No can do.'
+      }
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  city === 'NYC'? 'Ok sounds good.': 'No go.'
 }
 
 function switchOnCharmFromTip(){
